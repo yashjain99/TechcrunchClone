1e6b86e42b747f92cb5163e657c9ea2e78f6b6e3
unknown <jkaushik0802@gmail.com>
2021-02-03 21:36:49 +0530

    created events branch

--- techcrunch-clone/src/App.js
+++ techcrunch-clone/src/App.js
@@ -1,36 +1,9 @@
-<<<<<<< HEAD
-import logo from "./logo.svg";
-import "./App.css";
-import SideBar from "./Pages/SideBar/Components/SideBar";
-import EventsPage from "./Pages/Events/Components/EventsPage";
-
-import "./App.css";
-import Login from "./Pages/Login/components/Login";
-
-function App() {
-  return (
-    <div className="App">
-      <SideBar />
-
-      <Login />
-=======
-import { Home } from "./Pages/Homepage/Components/Home";
-import SideBar from "./Pages/SideBar/Components/SideBar";
-import Login from './Pages/Login/Components/Login';
-import FooterPage from "./Pages/Footer/Components/FooterPage";
+import Routes from "./Routes/Routes";
 
 function App() {
   return (
     <div>
-
-      {/* <SideBar />
-      <Home /> */}
-
-    
-    <Login/>
-    {/* <FooterPage/> */}
-
->>>>>>> a39f8dbe290cf6468ca46c82a5533bd273fcb974
+      <Routes />
     </div>
   );
 }
