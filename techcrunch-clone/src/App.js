4755010401b7ed9d15524cd01fcc9ce5a71b0c36
unknown <jkaushik0802@gmail.com>
2021-02-03 16:02:11 +0530

    changed css in App.js

--- techcrunch-clone/src/App.js
+++ techcrunch-clone/src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SideBar from "./Pages/SideBar/Components/SideBar";
+import EventsPage from "./Pages/Events/Components/EventsPage";
 
 function App() {
   return (
     <div className="App">
       <SideBar />
+      <div style={{ marginLeft: "250px" }}>
+        <EventsPage />
+      </div>
     </div>
   );
 }
