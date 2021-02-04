b6c61d712c5ced2bcf7ee14b8c67e007fe9c997b
unknown <jkaushik0802@gmail.com>
2021-02-04 17:15:27 +0530

    created events page

--- techcrunch-clone/src/Routes/Routes.jsx
+++ techcrunch-clone/src/Routes/Routes.jsx
@@ -5,19 +5,30 @@ import Login from "../Pages/Login/Components/Login";
 import SideBar from "../Pages/SideBar/Components/SideBar";
 import { Home } from "../Pages/Homepage/Components/Home";
 
+import EventsPage from "../Pages/Events/Components/EventsPage";
+
 function Routes() {
   return (
     <>
-      <Route exact path="/" render={(props) => <SideBar {...props} />} />
-      <Switch>
-        <Route exact path="/login" render={(props) => <Login {...props} />} />
-        <Route exact path="/" render={(props) => <Home {...props} />} />
-        <Route
-          render={() => (
-            <h2 style={{ textAlign: "center" }}>Page not found...</h2>
-          )}
-        />
-      </Switch>
+      <SideBar />
+      {/* <Route exact path="/" render={(props) => <SideBar {...props} />} /> */}
+      <div style={{ marginLeft: "250px", marginRight: "300px" }}>
+        <Switch>
+          <Route exact path="/login" render={(props) => <Login {...props} />} />
+          <Route exact path="/" render={(props) => <Home {...props} />} />
+          <Route
+            exact
+            path="/events/1"
+            render={(props) => <EventsPage {...props} />}
+          />
+
+          <Route
+            render={() => (
+              <h2 style={{ textAlign: "center" }}>Page not found...</h2>
+            )}
+          />
+        </Switch>
+      </div>
       {/* Footer */}
     </>
   );
